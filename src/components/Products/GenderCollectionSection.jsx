3618d9b8c21641234039b3bd73d8bcc4782d1a63
Abdel4527 <manaemam2003@gmail.com>
2025-04-02 19:51:42 -0400

    Fix

--- src/components/Products/GenderCollectionSection.jsx
+++ src/components/Products/GenderCollectionSection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import mensCollectionImage from '../../assets/mens-Collection.webp';
-import womensCollectionImage from '../../assets/womens-Collection.webp';
+import mensCollectionImage from '../../assets/mens-collection.webp';
+import womensCollectionImage from '../../assets/womens-collection.webp';
 import { Link } from 'react-router-dom';
 
 const GenderCollectionSection = () => {
